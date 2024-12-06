6b9f104ca48f7ffe2381985254132b5cd467b7f9
Thies Oelke <thies.oelke@gmail.com>
2024-12-06 18:23:16 +0100

    fixes ormconfig.ts file and npm scripts to use typeorm 0.3.x

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
 // eslint-disable-next-line
 const cookieSession = require('cookie-session');
--- typeormconfig.ts
+++ typeormconfig.ts
@@ -1,17 +1,19 @@
+// @ts-nocheck
 import { registerAs } from '@nestjs/config';
-import { config as dotenvConfig } from 'dotenv';
 import { Report } from './src/reports/report.entity';
 import { User } from './src/users/user.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { config as dotenvConfig } from 'dotenv-flow';
 
-dotenvConfig({ path: '.env' });
+dotenvConfig();
 
-const config = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  synchronize: false,
+const config: DataSourceOptions = {
+  database: process.env.DB_NAME,
+  type: process.env.DB_TYPE,
+  synchronize: process.env.DB_SYNC === 'true',
   entities: [User, Report],
+  migrations: [__dirname + '/dist/src/migrations/*{.ts,.js}'],
+  migrationsRun: process.env.DB_MIGRATIONS_RUN === 'true',
 };
-
 export default registerAs('typeorm', () => config);
 export const connectionSource = new DataSource(config as DataSourceOptions);
